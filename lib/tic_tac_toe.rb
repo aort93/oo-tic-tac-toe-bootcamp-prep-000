bde0601db67ee8c6e41d0cf0d65be2be9a8fc001
Alonso Ortega <alonsojortega@gmail.com>
2017-10-02 22:44:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -82,7 +82,7 @@ def won?
 
   def play
     turn until over?
-    puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
+    puts winner ? "Congratulations #{winner}!" : "Jr's Game!"
   end
 
 
